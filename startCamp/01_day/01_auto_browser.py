e6fe1c3e40fe7bc1d0007c568dfbf1e28150fd71
blueboy1593 <snb0303@naver.com>
2019-07-09 12:46:51 +0900

    StartCamp Day 01

---
+++ startCamp/01_day/01_auto_browser.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+idols = ['BTS', 'Twice', 'Itzy']
+
+for idol in idols:
+    webbrowser.open(f'http://google.com/search?q={idol}')
---
+++ startCamp/01_day/02_requests.py
@@ -0,0 +1,13 @@
+import requests
+import bs4 # Beautiful Soup
+
+url = 'https://finance.naver.com/sise/'
+
+response = requests.get(url)
+print(response)
+print(response.status_code)
+html = response.text
+
+soup = bs4.BeautifulSoup(html, 'html.parser')
+kospi = soup.select_one('#KOSPI_now').text
+print(kospi)
